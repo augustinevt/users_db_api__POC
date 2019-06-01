d1ce4e81d2732a4523fa39d2d9d1807f237acf14
august von trapp <augustvontrapp@gmail.com>
2019-06-01 12:46:50 -0700

    add books

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+  before_action :set_author, only: [:index, :show, :update, :destroy]
+  before_action :set_book, only: [:show, :update, :destroy]
+
+  # GET /books
+  def index
+    @books = @author.books.all
+
+    render json: @books
+  end
+
+  # GET /books/1
+  def show
+    render json: @book
+  end
+
+  # POST /books
+  def create
+    @book = @author.books.new(book_params)
+
+    if @book.save
+      render json: @book, status: :created, location: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /books/1
+  def update
+    if @book.update(book_params)
+      render json: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /books/1
+  def destroy
+    @book.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_book
+      @book = @author.books.find(params[:id])
+    end
+
+    def set_author
+      @author = Author.find(params[:author_id])
+    end
+end
